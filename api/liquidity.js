e4270aa45dacb5499d9d59c04583e09868a0f7b3
Aarav <pl46u3.ducklinz@gmail.com>
2022-07-09 15:35:14 +0530

    update

--- api/liquidity.js
+++ api/liquidity.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const router=express.Router();
 router.get("/",async (req,res)=>{
-    res.json({"hie":"UWU"})
+    res.json({"coins":10000,"funds":"1790"})
 })
 module.exports=router;
