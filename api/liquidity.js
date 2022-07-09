5c302534be83b2833987da7777da2bb26f0b2057
Aarav <pl46u3.ducklinz@gmail.com>
2022-07-09 15:40:42 +0530

    update headers

--- api/liquidity.js
+++ api/liquidity.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const router=express.Router();
 router.get("/",async (req,res)=>{
+    res.setHeader('Access-Control-Allow-Origin','*')
     res.json({"coins":10000,"funds":"1790"})
 })
 module.exports=router;
