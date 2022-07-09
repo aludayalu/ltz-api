e4673b2865f6f2f9bf0b60d0493f38e0075159a7
Aarav Dayal <97356263+aludayalu@users.noreply.github.com>
2022-07-09 20:29:59 +0530

    Update liquidity.js

--- api/liquidity.js
+++ api/liquidity.js
@@ -2,6 +2,6 @@ const express= require('express');
 const router=express.Router();
 router.get("/",async (req,res)=>{
     res.setHeader('Access-Control-Allow-Origin','*')
-    res.json({"coins":10000,"funds":"1790"})
+    res.json({"coins":10000,"funds":"2290"})
 })
 module.exports=router;
